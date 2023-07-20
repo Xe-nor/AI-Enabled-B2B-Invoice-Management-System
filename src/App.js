7c64c22e914866ae1fbccc7dd46eab669e15744e
Amarendra Dash <amarendra.amar.dash@gmail.com>
2023-07-20 11:23:02 +0530

    Frontend React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Headerr from "./components/Headerr.js";
+import Footerr from "./components/Footerr.js";
+
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Headerr />
+      <HomePage />
+      {/* <Trial /> */}
+      <Footerr />
+      {/* <DataGrid /> */}
+    </div>
+  );
+}
+
+export default App;
